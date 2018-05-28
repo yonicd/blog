ba827296b51445a3aad08fd5a9c12f2e922b5819
Jonathan Sidi <yonicd@gmail.com>
2018-05-28 04:55:54 -0400

    upadte about

--- github_traffic/github_traffic.R
+++ github_traffic/github_traffic.R
@@ -1,148 +1,77 @@
-library(RSelenium)
-library(ggedit)
-library(svglite)
-library(viridis)
-library(XML)
-library(ggplot2)
-library(plyr)
-library(dplyr)
-
-list2env(yaml::yaml.load_file('github_traffic/_ghcred.yml'),
-         envir = environment())
-
-repos <- c('shinyHeatmaply','regexSelect','rpdf','gunflow','lmmen',
-           'ggedit','slickR','sinew','d3Tree','texPreview','shinyCanvas',
-           'jsTree')
-
-gh_team <- rep(c('yonicd','metrumresearchgroup'),c(5,7))
-
-repos <- file.path(gh_team,repos)
-
-github_traffic <- function(gh_user,gh_pass,repos){
-
-rD <- rsDriver(verbose = FALSE)
-remDr <- rD[["client"]]
-
-remDr$navigate(sprintf('https://github.com/%s/graphs/traffic',repos[1]))
-webElem <- remDr$findElement(using = 'id', value = "login_field")
-webElem$setElementAttribute(attributeName = 'value',value = gh_user)
-webElem <- remDr$findElement(using = 'id', value = "password")
-webElem$setElementAttribute(attributeName = 'value',value = gh_pass)
-webElem=remDr$findElement(using = 'xpath','//*[@id="login"]/form/div[4]/input[3]')
-webElem$clickElement()
-Sys.sleep(1)
-
-out <- plyr::llply(repos,function(repo){
-  remDr$navigate(sprintf('https://github.com/%s/graphs/traffic',repo))
-  Sys.sleep(1)
-  out <- XML::htmlParse(remDr$getPageSource(),asText = TRUE)
-  sapply(c('clones','visitors'),function(type){
-  XML::getNodeSet(out,sprintf(sprintf('//*[@id="js-%s-graph"]/div/div[1]/svg/g/g',type)))
-},simplify = FALSE,USE.NAMES = TRUE)
-},.progress = 'text')
-
-names(out) <- repos
-
-remDr$close()
-rD[["server"]]$stop()
-
-return(out)
+fetch_view_type <- function(owner,repo,type,gh_pat){
+  
+  this_dat <- gh::gh('/repos/:owner/:repo/traffic/:type',
+                     owner  = owner,
+                     repo   = repo,
+                     type   = type,
+                     .token = gh_pat)
+  
+  
+  if(length(this_dat[[type]])==0)
+    return(NULL)
+  
+  ret <- this_dat[[type]]%>%
+    purrr::transpose()%>%
+    dplyr::as_tibble()%>%
+    dplyr::mutate(
+      timestamp = as.Date(gsub('T(.*?)$','',purrr::flatten_chr(timestamp))),
+      count  = purrr::flatten_dbl(count),
+      uniques  = purrr::flatten_dbl(uniques),
+      repo = repo
+    )
+  
+  ret
 }
 
-plot_data_html <- github_traffic(gh_user=gh_user,
-                            gh_pass=gh_pass,
-                            repos=repos)
-
-plot_data <- plyr::ldply(plot_data_html,function(repo){
-  plyr::mdply(names(repo),function(type){
-    
-    dat <- repo[[type]]
+fetch_view_data <- function(repo, gh_pat){
   
-    if(is.null(dat)) return(NULL)
-    
-    yticks_total <- as.numeric(sapply(getNodeSet(dat[[2]],'g'),XML::xmlValue))
-    y_mult_total <- as.numeric(gsub('[)]','',gsub('^(.*?),','',xmlAttrs(tail(getNodeSet(dat[[2]],'g'),1)[[1]])[2])))
-    
-    yticks_unique <- as.numeric(sapply(getNodeSet(dat[[5]],'g'),XML::xmlValue))
-    y_mult_unique <- as.numeric(gsub('[)]','',gsub('^(.*?),','',xmlAttrs(tail(getNodeSet(dat[[5]],'g'),1)[[1]])[2])))
-    
-    
-    x <- data.frame(type=type,
-                    date = as.Date(sapply(getNodeSet(dat[[1]],'g'),XML::xmlValue),format = '%m/%d'),
-                    total = as.numeric(sapply(getNodeSet(dat[[3]],'circle'),XML::xmlGetAttr,name = 'cy')),
-                    unique = as.numeric(sapply(getNodeSet(dat[[4]],'circle'),XML::xmlGetAttr,name = 'cy')))
+  types <- dplyr::data_frame(type = c('views','clones'))
 
-    x$total <- scales::rescale(x$total,rev(range(yticks_total))/((193-y_mult_total)/193))
-    x$unique <- scales::rescale(x$unique,rev(range(yticks_unique))/((193-y_mult_unique)/193))
-    
-    x%>%reshape2::melt(.,c('type','date'),variable.name=c('metric'))
-  })
-},.id='repo')%>%select(-X1)
+  ret <- purrr::map_df(types$type,
+                       fetch_view_type,
+                       gh_pat = gh_pat,
+                       owner  = dirname(repo),
+                       repo   = basename(repo),
+                       .id    = 'type')
+  
+  ret$type <- factor(ret$type,
+                     labels = c('views','clones')[1:max(ret$type)])
+  
+  return(ret)
+}
 
-
-# rD <- rsDriver(verbose = FALSE,port=4444L)
-# remDr <- rD$client
-# remDr$close()
-
-thiswd <- getwd()
-setwd('~/projects/blog')
-
-saveRDS(plot_data,file = sprintf('github_traffic/data/%s_gh_traffic.rds',
-                                 format(Sys.time(),format = '%Y%m%d_%H%M')))
-
-myfile <- list.files('github_traffic/data',full.names = TRUE)
-
-plot_data_df <- plyr::mdply(myfile,readRDS)
-
-levels(plot_data_df$X1) <- gsub('_gh(.*?)$','',basename(myfile))
-
-names(plot_data_df)[1] <- 'fetch_date'
-
-plot_data_df <- plot_data_df%>%
-  group_by(repo,type,date,metric)%>%
-  do(.,tail(.,1))%>%
-  ungroup()%>%
-  mutate(team=gsub('/(.*?)$','',repo),
-         repo=gsub('^(.*?)/','',repo),
-         val=ceiling(value))
-
-plot_list <- plyr::dlply(plot_data_df,c('type'),.fun = function(dat){
- p <- dat%>%
-    ggplot(aes(x=date,
+plot_traffic <- function(dat){
+  dat %>%
+    tidyr::gather(key = 'stat',value='value',count,uniques)%>%
+    ggplot(aes(x=timestamp,
                y=repo,
-               fill=val))+
+               fill=value))+
     geom_tile(colour='white',width=.95,alpha=0.75)+
     geom_hline(yintercept = c(0,(1:length(unique(dat$repo)))+0.5),colour='grey90')+
     scale_fill_viridis(name='Count',direction = -1)+
-    facet_grid(.~metric)+
-    #scale_x_date(date_breaks = "1 day",date_labels = "%m/%d")+
+    facet_grid(.~stat) +
     theme_minimal()+
     theme(panel.grid.major  = element_blank(),
           axis.text.x = element_text(angle=90),
           panel.grid.minor = element_blank(),
           axis.title = element_blank())
- 
- if(dat$type[1] =='clones'){
-   p <- p +
-     labs(title=sprintf('Github Team: %s | %s',paste0(unique(gh_team),collapse = ','),Sys.time()),
-          subtitle='Clones')
- }else{
-   p <- p +
-     labs(subtitle='Visitors')
- }
- 
- 
- p
-})
+}
 
-pl <- ggedit::as.gglist(plot_list)
+library(magrittr)
+
+yonicd_repos <- vcs::list_repos(user = 'yonicd')%>%purrr::flatten_chr()
+metrum_repos <- vcs::list_repos(user = 'metrumresearchgroup')%>%purrr::flatten_chr()
+metrum_repos <- metrum_repos[c(1:8,10)]
+
+x <- c(yonicd_repos,metrum_repos)%>%
+  purrr::map_df(fetch_view_data, 
+              gh_pat = Sys.getenv('GITHUB_PAT'))
+
 svglite(file.path(getwd(),'public/img/github_traffic.svg'),standalone = TRUE)
-print(pl,plot.layout=list(list(rows=1,cols=1:2),list(rows=2,cols=1:2)))
+plot_traffic(x)
 dev.off()
 
 system('git add public')
 system('git add github_traffic')
 system('git commit -m "update traffic"')
 system('git push origin master')
-
-setwd(thiswd)
