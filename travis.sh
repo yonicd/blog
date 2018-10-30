87a121cb940c243f4bcc50039c7a43f9946d20ef
Jonathan Sidi <yonis@metrumrg.com>
2018-10-30 07:16:22 -0400

    update travis.sh

--- travis.sh
+++ travis.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
   
 set -x
-if [ $TRAVIS_BRANCH == "master" ] ; then
 
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
@@ -15,7 +14,3 @@ git commit --message "Update Traffic Travis build: $TRAVIS_BUILD_NUMBER [skip ci
 
 git remote add deploy https://yonicd:${GITHUB_PAT}@github.com/yonicd/blog.git
 git push -f deploy traffic -v
-
-else
-echo "Not deploying, since this branch is not master."
-fi
