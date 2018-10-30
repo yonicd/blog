f22d24636d346a0d02b7ac5c1c3cfc734f1a1147
Jonathan Sidi <yonis@metrumrg.com>
2018-10-30 07:05:15 -0400

    fix travis.sh

--- travis.sh
+++ travis.sh
@@ -18,44 +18,4 @@ git push -f deploy traffic -v
 
 else
 echo "Not deploying, since this branch is not master."
-fi#!/bin/bash
-  
-set -x
-if [ $TRAVIS_BRANCH == "master" ] ; then
-
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "Travis CI"
-
-git checkout -b traffic
-
-Rscript -e "source('github_traffic/github_traffic.R')"
-
-git add .
-git commit --message "Update Traffic Travis build: $TRAVIS_BUILD_NUMBER [skip ci]"
-
-git remote add deploy https://yonicd:${GITHUB_PAT}@github.com/yonicd/blog.git
-git push -f deploy traffic -v
-
-else
-echo "Not deploying, since this branch is not master."
-fi#!/bin/bash
-  
-set -x
-if [ $TRAVIS_BRANCH == "master" ] ; then
-
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "Travis CI"
-
-git checkout -b traffic
-
-Rscript -e "source('github_traffic/github_traffic.R')"
-
-git add .
-git commit --message "Update Traffic Travis build: $TRAVIS_BUILD_NUMBER [skip ci]"
-
-git remote add deploy https://yonicd:${GITHUB_PAT}@github.com/yonicd/blog.git
-git push -f deploy traffic -v
-
-else
-echo "Not deploying, since this branch is not master."
 fi
