d9423f3c8768b32b98e5caa2a5128e9b502c148f
Jonathan Sidi <yonis@metrumrg.com>
2018-10-30 06:40:40 -0400

    create branch in travis

--- travis.sh
+++ travis.sh
@@ -6,13 +6,15 @@ if [ $TRAVIS_BRANCH == "master" ] ; then
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
+git checkout -b traffic
+
 Rscript -e 'pkgs = c("viridis","svglite","dplyr","purrr","ggplot2","gh","tidyr");x <- setdiff(pkgs,rownames(installed.packages()));if(length(x)>0) install.packages(x);source("github_traffic/github_traffic.R");'
 
 git add .
 git commit --message "Update Traffic Travis build: $TRAVIS_BUILD_NUMBER [skip ci]"
 
 git remote add deploy https://yonicd:${GITHUB_PAT}@github.com/yonicd/blog.git
-git push -f deploy master -v
+git push -f deploy traffic -v
 
 else
 echo "Not deploying, since this branch is not master."
