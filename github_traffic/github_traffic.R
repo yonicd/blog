128444bcd60c9d38c26aa4b334fce63c3880c6ba
Jonathan Sidi <yonis@metrumrg.com>
2018-10-29 22:41:27 -0400

    add .travis.yml

--- github_traffic/github_traffic.R
+++ github_traffic/github_traffic.R
@@ -93,5 +93,5 @@ dev.off()
 
 system('git add public')
 system('git add github_traffic')
-system('git commit -m "update traffic"')
+system('git commit -m "update traffic [skip ci]"')
 system('git push origin master')
